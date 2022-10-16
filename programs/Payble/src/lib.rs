d1c7b5d9e6f97c0624ff5bd30f7ac970e491453e
SamirJoshiGit <samirj523@Orion_22.localdomain>
2022-10-16 00:50:55 -0500

    last working version

--- programs/Payble/src/lib.rs
+++ programs/Payble/src/lib.rs
@@ -1,65 +1,65 @@
 use anchor_lang::prelude::*;
-use std::mem::size_of;
- 
-declare_id!("4MF6qojdvBhF9GSzQeCVFJ1it7FN3dN6d9DMGbmPwDGm");
- 
+
+declare_id!("F1imVqpXvC7FMw23C4LwZMmcvALybSZ8FzfUBqW1vcu4");
+
 #[program]
 pub mod payble {
     use super::*;
- 
-    pub fn create_table(ctx: Context<CreateTable>, name: String) -> Result<()> {
-        // Get Escrow Account
-        let table_account = &mut ctx.accounts.table_account;
- 
-        // set amount
-        table_account.amount = 0;
- 
-        table_account.name = name;
- 
+
+    pub fn create_ledger(
+        ctx: Context<CreateLedger>,
+        color: String,
+    ) -> Result<()> {
+
+        let ledger_account = &mut ctx.accounts.ledger_account;
+        ledger_account.color = color;
+        ledger_account.balance = 0;
+        
         Ok(())
     }
- 
-    pub fn modify_table(ctx: Context<CreateTable>, amount_added: u16)-> Result<()>{
-        let table_account = &mut ctx.accounts.table_account;
-        table_account.amount = table_account.amount + amount_added;
+
+    pub fn modify_ledger(
+        ctx: Context<ModifyLedger>,
+        new_balance: u32,
+    ) -> Result<()> {
+
+        let ledger_account = &mut ctx.accounts.ledger_account;
+        ledger_account.balance = ledger_account.balance + new_balance;
+        
         Ok(())
     }
- 
 }
- 
+
 #[derive(Accounts)]
-#[instruction(name: String)]
-pub struct CreateTable<'info> {
-    // Escrow Account PDA
+#[instruction(color: String)]
+pub struct CreateLedger<'info> {
     #[account(
         init,
         payer = wallet,
         space = 82,
-        seeds = [wallet.key().as_ref(), b"_", name.as_ref()],
-        bump,
+        seeds = [
+            wallet.key().as_ref(),
+            b"_",
+            color.as_ref(),
+        ],
+        bump
     )]
-    pub table_account: Account<'info, Table>,
- 
+    pub ledger_account: Account<'info, Ledger>,
     #[account(mut)]
     pub wallet: Signer<'info>,
- 
     pub system_program: Program<'info, System>,
 }
- 
+
 #[derive(Accounts)]
-pub struct ModifyTable<'info> {
+pub struct ModifyLedger<'info> {
     #[account(mut)]
-    pub table_account: Account<'info, Table>,
+    pub ledger_account: Account<'info, Ledger>,
     #[account(mut)]
-    pub wallet: Signer<'info>
+    pub wallet: Signer<'info>,
 }
- 
+
 #[account]
-pub struct Table {
-    // Amount that is owed by table_account
-    pub name: String,
-    //String of table_account name
-    pub amount: u16,
+pub struct Ledger {
+    pub color: String,
+    pub balance: u32,
 }
- 
- 
--- tests/Payble.ts
+++ tests/Payble.ts
@@ -2,100 +2,106 @@ import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { Payble } from "../target/types/payble";
  
+
 function shortKey(key: anchor.web3.PublicKey) {
   return key.toString().substring(0, 8);
 }
- 
-describe("Payble", () => {
- 
+
+
+describe("payble", () => {
+  
   const provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
-  const program = anchor.workspace.Payble as Program<Payble>;
- 
+  const program = anchor.workspace.Payble as anchor.Program<Payble>;
+
   async function generateKeypair() {
     let keypair = anchor.web3.Keypair.generate();
     await provider.connection.requestAirdrop(
       keypair.publicKey,
       2 * anchor.web3.LAMPORTS_PER_SOL
     );
-    await new Promise(resolve => setTimeout(resolve, 3 * 1000)); // Sleep 3s
+    await new Promise( resolve => setTimeout(resolve, 3 * 1000) ); // Sleep 3s
     return keypair;
   }
- 
-  async function derivePda(name: string, pubkey: anchor.web3.PublicKey) {
+
+  async function derivePda(color: string, pubkey: anchor.web3.PublicKey) {
     let [pda, _] = await anchor.web3.PublicKey.findProgramAddress(
       [
         pubkey.toBuffer(),
         Buffer.from("_"),
-        Buffer.from(name)
+        Buffer.from(color),
       ],
       program.programId
     );
     return pda;
   }
- 
-  async function createTableAccount(
-    name: string,
-    pda: anchor.web3.PublicKey,
+
+  async function createLedgerAccount(
+    color: string, 
+    pda: anchor.web3.PublicKey, 
     wallet: anchor.web3.Keypair
   ) {
-    await program.methods.createTable(name)
+    await program.methods.createLedger(color)
       .accounts({
-        tableAccount: pda,
+        ledgerAccount: pda,
         wallet: wallet.publicKey,
       })
       .signers([wallet])
       .rpc();
   }
- 
-  async function modifyTable(
-    name: string,
-    amountadd: number,
-    wallet: anchor.web3.Keypair,
+
+  async function modifyLedger(
+    color: string, 
+    newBalance: number,
+    wallet: anchor.web3.Keypair, 
   ) {
- 
+
     console.log("--------------------------------------------------");
     let data;
-    let pda = await derivePda(name, wallet.publicKey);
- 
-    console.log(`Checking if account ${shortKey(pda)} exists for name: ${name}...`);
+    let pda = await derivePda(color, wallet.publicKey);
+
+    console.log(`Checking if account ${shortKey(pda)} exists for color: ${color}...`);
     try {
- 
-      data = await program.account.table.fetch(pda);
+
+      data = await program.account.ledger.fetch(pda);
       console.log("It does.");
- 
+    
     } catch (e) {
- 
+    
       console.log("It does NOT. Creating...");
-      await createTableAccount(name, pda, wallet);
-      data = await program.account.table.fetch(pda);
+      await createLedgerAccount(color, pda, wallet);
+      data = await program.account.ledger.fetch(pda);
     };
- 
+
     console.log("Success.");
     console.log("Data:")
-    console.log(`    Name: ${data.name}   Balance: ${data.amount}`);
-    console.log(`Modifying balance of ${data.name} from ${data.amount} to ${amountadd}`);
- 
-    await program.methods.modifyTable(amountadd).accounts({
-      tableAccount: pda,
-      wallet: wallet.publicKey,
-    });
- 
-    data = await program.account.table.fetch(pda);
+    console.log(`    Color: ${data.color}   Balance: ${data.balance}`);
+    console.log(`Modifying balance of ${data.color} from ${data.balance} to ${newBalance}`);
+
+    await program.methods.modifyLedger(newBalance)
+      .accounts({
+        ledgerAccount: pda,
+        wallet: wallet.publicKey,
+      })
+      .signers([wallet])
+      .rpc();
+
+    data = await program.account.ledger.fetch(pda);
     console.log("New Data:")
-    console.log(`    Name: ${data.name}   Balance: ${data.amount}`);
+    console.log(`    Color: ${data.color}   Balance: ${data.balance}`);
     console.log("Success.");
   }
- 
-    it("An example of PDAs in action", async () => {
- 
-      const testKeypair1 = await generateKeypair();
- 
-      await modifyTable("table1", 4, testKeypair1);
-      await modifyTable("table1", 2, testKeypair1);
- 
-      const testKeypair2 = await generateKeypair();
-      await modifyTable("red", 3, testKeypair2);
-      await modifyTable("green", 3, testKeypair2);
-    });
+
+
+  it("OUR GODAMN PDA", async () => {
+
+    const testKeypair1 = await generateKeypair();
+    await modifyLedger("red", 2, testKeypair1);
+    await modifyLedger("red", 4, testKeypair1);
+    await modifyLedger("blue", 2, testKeypair1);
+
+    const testKeypair2 = await generateKeypair();
+    await modifyLedger("red", 3, testKeypair2);
+    await modifyLedger("green", 3, testKeypair2);
   });
+});
